f318c39ed5d8c235fce4bdca1d0da29764f8e328
Lawrence <lawrenceshah1@gmail.com>
2016-06-19 18:29:24 -0700

    removed php from stocks app
    github pages doesn't support php

--- stocks/services/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-	define("DB_HOST", "localhost");
-	define("DB_USER", "webAppUser");
-	define("DB_PASS", "webAppPass");
-	define("DB_NAME", "webApp");
-	
-	define("TOKEN_KEY", "changeMeLater");
-	define("TOKEN_LIFESPAN", 600);
-?>
