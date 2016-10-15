5c6e17dfb59f000c512c2b43390370794bea57d6
Lawrence <lawrence.webdev@gmail.com>
2016-10-15 16:13:05 -0700

    updated with sherry's help
    changed wording
    changed fonts
    changed/added background images
    change opacity of project tiles

--- js/main.js
+++ js/main.js
@@ -1,16 +1,10 @@
-(function() {
-	$('.navbar-link').add('.navbar-brand').on('click', function(e) {
-		e.preventDefault();
-		$('html,body').stop().animate({scrollTop:$($(this).attr('href')).offset().top-50}, 500);
-		$('.navbar-collapse').removeClass('in');
-	});
-	
-	$('.section-top-arrow').on('click', function() {
-		$('html,body').stop().animate({scrollTop:$('#section-about').offset().top-50}, 500);
-		$('.navbar-collapse').removeClass('in');
-	});
-	
-	$('.section-projects-tile').on('touchend', function() {
-		$(this).toggleClass('active');
-	});
-})();
+$('.navbar-link').add('.navbar-brand').on('click', function(e) {
+	e.preventDefault();
+	$('html,body').stop().animate({scrollTop:$($(this).attr('href')).offset().top-50}, 500);
+	$('.navbar-collapse').removeClass('in');
+});
+
+$('.section-top-arrow').on('click', function() {
+	$('html,body').stop().animate({scrollTop:$('#section-about').offset().top-50}, 500);
+	$('.navbar-collapse').removeClass('in');
+});
